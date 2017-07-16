b9676fde22452fe8af8a561f0c14c78fbf4b23e3
Bruce <tryagaingamez@gmail.com>
2017-07-16 09:41:01 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance;
+	public bool gameOver = false;
+	public float scrollSpeed = -1.5f;
+	private int score = 0;
+	public Text scoreText;
+	public Button[] buttons;
+
+	// Use this for initialization
+	void Awake () {
+
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update (){
+
+}	
+
+	public void BirdScored(){
+		if (gameOver) {
+			return;
+		}
+		score++;
+		scoreText.text = "Score: " + score.ToString ();
+
+	}
+	public void BirdDied(){
+		gameOver = true;
+		foreach (Button button in buttons) {
+			button.gameObject.SetActive (true);
+
+		}
+}
+
+	public void Play(){
+		SceneManager.LoadScene("one");
+	}
+	public void Menu(){
+		SceneManager.LoadScene ("menu");
+	}
+	public void Exit(){
+		Application.Quit ();
+
+	}
+
+}
---
+++ UiManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+	public Button[] buttons;
+	bool gameOver;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void gameOverActivated(){
+		gameOver = true;
+		foreach (Button button in buttons) {
+			button.gameObject.SetActive(true);
+		}
+	}
+
+
+	public void Play(){
+		SceneManager.LoadScene ("one");	
+	}
+
+	public void Pause(){
+
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+		} 
+		else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+		}
+	}
+	public void Menu(){
+
+		SceneManager.LoadScene ("menu");
+	}
+	public void Exit(){
+		Application.Quit ();
+
+	}
+}
